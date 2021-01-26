2314edfd228493f0092a873851da05f38ec4dcfb
Deadlift3008 <deadlift3008@gmail.com>
2021-01-26 22:14:18 +0300

    01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,9 @@
-function sum(a, b) {
-  /* ваш код */
-}
+const throwWrongTypeError = () => {
+  throw new TypeError('Wrong type');
+};
+
+const sum = (...args) => args.every((num) => Number.isInteger(num)) ?
+    args.reduce((res, item) => res + item) :
+    throwWrongTypeError();
 
 module.exports = sum;
