eefda28815ee01cd2ee35f6bf0d0490c3ed6ce03
Deadlift3008 <deadlift3008@gmail.com>
2021-02-05 00:30:48 +0300

    limit size stream

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -4,10 +4,15 @@ const LimitExceededError = require('./LimitExceededError');
 class LimitSizeStream extends stream.Transform {
   constructor(options) {
     super(options);
+
+    this.limit = options.limit;
+    this.currentSize = 0;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.currentSize += Buffer.byteLength(chunk.toString(), 'utf8');
+    const error = this.currentSize > this.limit ? new LimitExceededError() : null;
+    callback(error, chunk);
   }
 }
 
