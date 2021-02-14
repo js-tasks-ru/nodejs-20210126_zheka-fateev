b539d3989208527b3d0b420eb3213b4d3255033f
Deadlift3008 <deadlift3008@gmail.com>
2021-02-15 01:01:24 +0300

    05-01 file-server-get

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,17 +1,38 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
+  if (pathname && pathname.indexOf('/') !== -1) {
+    res.statusCode = 400;
+    res.end(`Nested path does not supported`);
+
+    return;
+  }
+
   const filepath = path.join(__dirname, 'files', pathname);
 
   switch (req.method) {
     case 'GET':
+      fs.access(filepath, fs.F_OK, (err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end(`Cannot find file ${pathname}`);
+          return;
+        }
 
+        const stream = fs.createReadStream(filepath);
+        stream.pipe(res);
+        stream.on('error', (err) => {
+          res.statusCode = 500;
+          res.end('Unknown error: ' + err.message);
+        });
+      });
       break;
 
     default:
