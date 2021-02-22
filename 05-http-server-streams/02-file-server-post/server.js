e3294984e8c75b95de7462ee1b090181a7b8a07e
Deadlift3008 <deadlift3008@gmail.com>
2021-02-22 15:16:46 +0300

    file server post

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('../../03-streams/01-limit-size-stream/LimitSizeStream');
 
 const server = new http.Server();
 
@@ -9,8 +11,55 @@ server.on('request', (req, res) => {
 
   const filepath = path.join(__dirname, 'files', pathname);
 
+  if (pathname && pathname.indexOf('/') !== -1) {
+    res.statusCode = 400;
+    res.end(`Nested path does not supported`);
+
+    return;
+  }
+
   switch (req.method) {
     case 'POST':
+      const stream = fs.createWriteStream(filepath, {flags: 'wx'});
+      const limitStream = new LimitSizeStream({limit: 1e6});
+
+      req.pipe(limitStream).pipe(stream);
+
+      stream.on('error', (error) => {
+        if (error.code === 'ENOENT') {
+          res.statusCode = 404;
+          res.end();
+        }
+
+        if (error.code === 'EEXIST') {
+          res.statusCode = 409;
+          res.end('File exists');
+        }
+      }).on('finish', () => {
+        res.statusCode = 201;
+        res.end('file has been saved');
+      });
+
+      req.on('error', () => {
+        fs.unlink(filepath, (error) => {
+          stream.end();
+          res.end();
+        });
+      });
+
+      req.on('aborted', () => {
+        stream.destroy();
+        limitStream.destroy();
+      });
+
+      limitStream.on('error', (error) => {
+        if (error.code === 'LIMIT_EXCEEDED') {
+          fs.unlink(filepath, () => {});
+          stream.end();
+          res.statusCode = 413;
+          res.end('File is too big');
+        }
+      });
 
       break;
 
